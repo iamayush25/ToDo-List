10a970ba560a7cefccb0c78d65f039a75fd500e1
Ayush Sharma <ayush22@navgurukul.org>
2024-05-21 20:06:06 +0530

    pass props and get data

--- src/Components/Data.jsx
+++ src/Components/Data.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './App.css';
 
-function Data({AllData}) {
+function Data({AllData , deleteItem , id , editItemPrompt} ) {
 
   return (
     <>
       <li className='todo-list'>
         {AllData}
         <div>
-          <button className='Delete-button'><img src={require('./Image/Edit-button.png')} alt="" /></button>
-          <button className='Delete-button'><img src={require('./Image/delete-button.png')} alt="" /></button>
+          <button className='Edit-button'><img src={require('./Image/Edit-button.png')} alt="Edit Button" 
+          onClick={() => editItemPrompt(id)} /></button>
+          <button className='Delete-button'><img src={require('./Image/delete-button.png')} alt="Delete Button" onClick={() => deleteItem(id)} /></button>
         </div>
       </li>
 
