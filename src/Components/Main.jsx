7b18032982ead70ba070b4d3d243bb6c71ced8a2
Ayush Sharma <ayush22@navgurukul.org>
2024-05-24 23:25:42 +0530

    Data Store In an object

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,66 +1,83 @@
-import React, { useState } from 'react'
-import './App.css'
-import Data from './Data'
+import React, { useState } from 'react';
+import './App.css';
+import Data from './Data';
 
 function Main() {
-    const [ListItem, setListItem] = useState('')
-    const [userList, setuserList] = useState([])
+    const [itemName, setItemName] = useState('');
+    const [contact, setContact] = useState('');
+    const [cityName, setCityName] = useState('');
+    const [userList, setUserList] = useState([]);
+
     const getData = () => {
-        setuserList((previousData) => {
-            return [...previousData, ListItem]
-        })
-        setListItem("")
-    }
-
-    const deleteData = (id) => {
-        console.log('item deleted');
-        setuserList((previousData) => {
-            return previousData.filter((ArrayElements, index) => {
-                return index !== id
-            })
-        })
-    }
-
-    const editItemPrompt = (id) => {
-        const newName = prompt('Enter the new name:');
-        if (newName) {
-            const index = userList.findIndex(userList => userList.id === id);
-            if (index !== -1) {
-                const newItems = [...userList];
-                newItems[index] = { ...setuserList[index], name: newName };
-                setuserList(newItems);
-            }
+        if (itemName.trim() === '' || contact.trim() === '' || cityName.trim() === '') {
+            alert('Please fill in all fields.');
+            return;
         }
-    }
 
-    const ItemsEvent = (event) => {
-        setListItem(event.target.value)
-    }
+        setUserList(prevData => [
+            ...prevData,
+            {
+                name: itemName,
+                contact: contact,
+                city: cityName
+            }
+        ]);
+
+        setItemName('');
+        setContact('');
+        setCityName('');
+    };
+
+    const deleteData = (index) => {
+        setUserList(prevData => prevData.filter((_, i) => i !== index));
+    };
+
+    const editData = (index) => {
+        const newName = prompt("Edit name:", userList[index].name);
+        const newContact = prompt("Edit contact:", userList[index].contact);
+        const newCity = prompt("Edit city:", userList[index].city);
+
+        if (newName && newContact && newCity) {
+            const updatedList = [...userList];
+            updatedList[index] = { name: newName, contact: newContact, city: newCity };
+            setUserList(updatedList);
+        }
+    };
+
     return (
-        <>
-            <div className='Main-Container'>
-                <div className='title'>
-                    <h1>ToDo List</h1>
-                </div>
-                <div className='todo-body' >
-                    <div className='inputs'>
-                        <input type='text' placeholder='Add Name' value={ListItem} onChange={ItemsEvent} />
-                        <button onClick={getData} >Add</button>
-                    </div>
-                    <ul className='todo-item'>
-                        {userList.map((FinalData, index) => {
-                            return <Data id={index} key={index}    // props
-                                deleteItem={deleteData}           // props
-                                editItemPrompt = {editItemPrompt}  // props
-                                AllData={FinalData} />           // props
-                        })}
-
-                    </ul>
-                </div>
-
+        <div className='Main-Container'>
+            <div className='title'>
+                <h1>ToDo List</h1>
             </div>
-        </>
-    )
+            <div className='todo-body'>
+                <div className='inputs'>
+                    <input type='text' placeholder='Add Name' value={itemName} onChange={(e) => setItemName(e.target.value)} />
+                    <input type='text' placeholder='Add Contact' value={contact} onChange={(e) => setContact(e.target.value)} />
+                    <input type='text' placeholder='Add City' value={cityName} onChange={(e) => setCityName(e.target.value)} />
+                    <button onClick={getData}>Add</button>
+                </div>
+                <ul className='todo-item'>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Contact</th>
+                                <th>City</th>
+                            </tr>
+                        </thead>
+                    </table>
+                    {userList.map((item, index) => (
+                        <Data
+                            key={index}
+                            AllData={item}
+                            deleteItem={() => deleteData(index)}
+                            editPrompt={() => editData(index)}
+                        />
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
-export default Main
+export default Main;
