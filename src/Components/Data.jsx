6d12225ece3acf6edef65e0cb5a8bbe8f195cfa0
Ayush Sharma <ayush22@navgurukul.org>
2024-05-24 23:25:14 +0530

    Add Data In table

--- src/Components/Data.jsx
+++ src/Components/Data.jsx
@@ -1,21 +1,36 @@
-import React from 'react'
+import React from 'react';
 import './App.css';
 
-function Data({AllData , deleteItem , id , editItemPrompt} ) {
-
+function Data({ AllData, deleteItem, id, editPrompt }) {
   return (
     <>
       <li className='todo-list'>
-        {AllData}
-        <div>
-          <button className='Edit-button'><img src={require('./Image/Edit-button.png')} alt="Edit Button" 
-          onClick={() => editItemPrompt(id)} /></button>
-          <button className='Delete-button'><img src={require('./Image/delete-button.png')} alt="Delete Button" onClick={() => deleteItem(id)} /></button>
-        </div>
+        <table>
+          <tbody>
+            <tr>
+              <td>{AllData.name}</td>
+              <td>{AllData.contact}</td>
+              <td>{AllData.city}</td>
+            </tr>
+            <button className='Edit-button' onClick={() => editPrompt(id)}>
+            <img src={require('./Image/Edit-button.png')} alt="Edit Button" />
+          </button>
+          <button className='Delete-button' onClick={() => deleteItem(id)}>
+            <img src={require('./Image/delete-button.png')} alt="Delete Button" />
+          </button>
+          </tbody>
+        </table>
+        {/* <div>
+          <button className='Edit-button' onClick={() => editPrompt(id)}>
+            <img src={require('./Image/Edit-button.png')} alt="Edit Button" />
+          </button>
+          <button className='Delete-button' onClick={() => deleteItem(id)}>
+            <img src={require('./Image/delete-button.png')} alt="Delete Button" />
+          </button>
+        </div> */}
       </li>
-
     </>
-  )
+  );
 }
 
 export default Data;
